51615bb003f12e4d0496d95a73ab3ba262d0bddf
bhudelson <bhudelso@gmail.com>
2016-03-17 00:43:18 -0700

    Project Update
    updated auto layout, gif on start screen, walk thru connection to game
    controller

---
+++ Bluffer/BeginViewController.swift
@@ -0,0 +1,71 @@
+//
+//  BeginViewController.swift
+//  Bluffer
+//
+//  Created by Blake Hudelson on 3/16/16.
+//  Copyright © 2016 Blake Hudelson. All rights reserved.
+//
+
+import UIKit
+
+class BeginViewController: UIViewController {
+
+    @IBOutlet weak var imagesView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var imagesNames = ["guy1", "guy2", "guy3", "guy4"]
+        
+        
+        var images = [UIImage]()
+        
+        
+        for i in 0..<imagesNames.count{
+            
+            images.append(UIImage(named: imagesNames[i])!)
+            
+        }
+        
+        imagesView.animationImages = images
+        imagesView.animationDuration = 3
+        imagesView.startAnimating()
+        
+    }
+
+//    override func viewDidAppear(animated: Bool) {
+//    
+//        var imagesNames = ["guy01", "guy02", "guy03", "guy04"]
+//        
+//        var images = [UIImage]()
+//        
+//        
+//        for i in 0..<imagesNames.count{
+//            
+//            images.append(UIImage(named: imagesNames[i])!)
+//            
+//        }
+//        
+//        imagesView.animationImages = images
+//        imagesView.animationDuration = 1.0
+//        imagesView.startAnimating()
+//        
+//    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
