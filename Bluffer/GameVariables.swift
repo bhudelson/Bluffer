59f224f9633eae57a0d210f4f40b80a62b1d5904
bhudelson <bhudelso@gmail.com>
2016-07-15 17:59:26 -0700

    update

--- Bluffer/GameVariables.swift
+++ Bluffer/GameVariables.swift
@@ -22,7 +22,7 @@ var team1Score = 0
 var team2Score = 0
 
 //Duration in seconds of image guess time
-var gameTime = 1
+var gameTime = 20
 
 //Used to track whether a team scored a point in the current round
 var correctGuess : Bool = false
